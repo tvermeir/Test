e4aab414b7b0836b51f1a6a15916c66a0d3aa13b
Todd Vermeir <todd.vermeir@gmail.com>
2023-04-06 15:36:29 +1200

    Hello

---
+++ PracticeMode/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	String day = "Monday";
+		
+	switch(day) {
+		case "Sunday": System.out.println("It is sunday!");
+		break;
+		
+		case "Monday": System.out.println("It is Mondag!");
+	
+	}
+	}
+
+}
